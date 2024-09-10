7ea6b893ec2fa741756406f7424bd910b772b776
jme <jme.co.jp@gmail.com>
2024-09-10 12:04:48 +0900

    v4l2

---
+++ launch/launch.py
@@ -0,0 +1,19 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    return LaunchDescription(
+        [
+            Node(
+                package="v4l2_camera",
+                executable="v4l2_camera_node",
+            ),
+            Node(
+                package="image_transport",
+                executable="republish",
+                arguments=["raw"],
+                remappings=[("in", "/image_raw"), ("out", "/image_raw/compressed")],
+            ),
+        ]
+    )
