61e6104473b9cd6e90986edf193e74c08361980c
jme <jme.co.jp@gmail.com>
2024-09-10 16:56:01 +0900

    Update image_transport republish arguments to use "raw" instead of "compressed"

--- launch/launch.py
+++ launch/launch.py
@@ -9,11 +9,11 @@ def generate_launch_description():
                 package="v4l2_camera",
                 executable="v4l2_camera_node",
             ),
-            Node(
-                package="image_transport",
-                executable="republish",
-                arguments=["raw"],
-                # remappings=[("in", "/image_raw"), ("out", "/image_raw/compressed")],
-            ),
+            # Node(
+            #     package="image_transport",
+            #     executable="republish",
+            #     arguments=["compressed"],
+            #     remappings=[("in", "/image_raw"), ("out", "/image_raw/compressed")],
+            # ),
         ]
     )
