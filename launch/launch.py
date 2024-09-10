1602286cb2e1b60d7854fefd60a7bb525064a2df
jme <jme.co.jp@gmail.com>
2024-09-10 18:18:23 +0900

    Update launch.py to remove commented out image_transport republish node

--- launch/launch.py
+++ launch/launch.py
@@ -9,11 +9,5 @@ def generate_launch_description():
                 package="v4l2_camera",
                 executable="v4l2_camera_node",
             ),
-            # Node(
-            #     package="image_transport",
-            #     executable="republish",
-            #     arguments=["compressed"],
-            #     remappings=[("in", "/image_raw"), ("out", "/image_raw/compressed")],
-            # ),
         ]
     )
